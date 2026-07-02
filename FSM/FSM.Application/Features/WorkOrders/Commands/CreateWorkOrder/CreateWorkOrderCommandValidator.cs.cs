3023bdaa4e4086138cbe191bf9664d3edd4e7c68
MEHMET EMRE TEK <2023123064@cumhuriyet.edu.tr>
2026-07-02 16:12:56 +0300

    feat: work order olusturma altyapisi, fluent validation ve yetkilendirme eklendi

---
+++ FSM/FSM.Application/Features/WorkOrders/Commands/CreateWorkOrder/CreateWorkOrderCommandValidator.cs.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace FSM.Application.Features.WorkOrders.Commands.CreateWorkOrder;
+
+public class CreateWorkOrderCommandValidator : AbstractValidator<CreateWorkOrderCommand>
+{
+    public CreateWorkOrderCommandValidator()
+    {
+        RuleFor(x => x.CustomerId).GreaterThan(0).WithMessage("Lütfen geçerli bir müşteri seçin.");
+
+        // Teknisyen kuralını sildik, yerine başlık kuralı ekledik
+        RuleFor(x => x.Title).NotEmpty().WithMessage("İş emri başlığı boş bırakılamaz.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("İş emri açıklaması boş bırakılamaz.")
+            .MinimumLength(10).WithMessage("Açıklama çok kısa, lütfen en az 10 karakterlik bir detay girin.");
+    }
+}
